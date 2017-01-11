3f694430e982aba39fd45ddd9f6960b731789df9
slightair <arksutite@gmail.com>
2017-01-12 00:13:47 +0900

    Create project

---
+++ akane/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  akane
+//
+//  Created by slightair on 2017/01/12.
+//  Copyright © 2017 slightair. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
