a05392dbd1dc4ef4134da0302d75bcc5045b49b3
slightair <arksutite@gmail.com>
2017-01-17 00:40:02 +0900

    Refresh access token when expired

--- akane/Reddit/RedditService.swift
+++ akane/Reddit/RedditService.swift
@@ -6,6 +6,8 @@ import KeychainAccess
 class RedditService {
     static let shared = RedditService()
 
+    let clientID: String
+
     private var keychain: Keychain {
         let service = "\(Bundle.main.bundleIdentifier!).reddit"
         return Keychain(service: service)
@@ -23,7 +25,15 @@ class RedditService {
         return keychain["accessToken"]
     }
 
+    private var refreshToken: String? {
+        return keychain["refreshToken"]
+    }
+
     private(set) var currentUser: RedditUser?
+
+    init(clientID: String = RedditServiceConfiguration.clientID) {
+        self.clientID = clientID
+    }
 
     func fetchUserInfo(credential: RedditCredential) -> Observable<RedditUser> {
         storeCredential(credential)
@@ -35,8 +45,28 @@ class RedditService {
         let request = RedditAPI.UserRequest()
         return Session.shared.rx
             .response(request)
+            .retryWhen { (errors: Observable<Error>) in
+                return errors.flatMapWithIndex { error, retryCount -> Observable<RedditCredential> in
+                    if case SessionTaskError.responseError(ResponseError.unacceptableStatusCode(401)) = error, retryCount < 1 {
+                        return self.refreshAccessToken()
+                    }
+                    return Observable.error(error)
+                }
+            }
             .do(onNext: { user in
                 self.currentUser = user
+            })
+    }
+
+    func refreshAccessToken() -> Observable<RedditCredential> {
+        guard let refreshToken = refreshToken else {
+            fatalError("refresh token not found")
+        }
+
+        let request = RedditAPI.AccessTokenRequest(clientID: clientID, refreshToken: refreshToken)
+        return Session.shared.rx.response(request)
+            .do(onNext: { credential in
+                self.storeCredential(credential)
             })
     }
 
